c9d4876abb99276dde1941a086e65561d481c2b9
cojun <dhkdzh70@gmail.com>
2023-02-09 14:46:43 +0900

    time add

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def home():
 def test():
    return render_template('test.html')
 
+@app.route('/final')
+def final():
+   return render_template('final.html')
+
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
